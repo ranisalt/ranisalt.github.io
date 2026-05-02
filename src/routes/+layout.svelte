ec165cffc19d574eb4f7d735ed808df108560830
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2026-05-02 04:00:17 +0200

    chore: remove unused font links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,11 @@
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
 
-  let { children } = $props();
+  const { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Instrument+Serif:ital@0;1&family=JetBrains+Mono:wght@400;500&display=swap"
-  />
 </svelte:head>
+
 {@render children()}
